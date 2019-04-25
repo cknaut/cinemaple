5903ca79abd30664b115629d706617469d763dad
cknaut <can_knaut@yahoo.de>
2019-04-25 18:45:47 +0200

    Debug False

--- cinemaple/settings.py
+++ cinemaple/settings.py
@@ -54,10 +54,14 @@ RECAPTCHA_PUBLIC_KEY = os.environ['RECAPTCHA_SITE_KEY']
 # automatically run in debug mode if in production
 environement = os.environ['DJANGO_ENV']
 
+'''
 if environement == "DEBUG":
     DEBUG = True
 elif environement == "PRODUCTION":
     DEBUG = False
+'''
+
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -77,13 +81,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'userhandling.middleware.UserBasedExceptionMiddleware',
 ]
 
