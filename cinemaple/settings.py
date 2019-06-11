70ba2e035edea684adbc3ec94f407295cd40f860
cknaut <can_knaut@yahoo.de>
2019-06-11 23:21:55 +0200

    Collecstatic locally, changed static root.

--- cinemaple/settings.py
+++ cinemaple/settings.py
@@ -168,7 +168,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'cinemaple/staticfiles')
 STATIC_URL = '/static/'
 
 
